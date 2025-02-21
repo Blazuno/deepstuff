79eb493b0f51b9677d9984dfff0f97af8561a5d3
Blazuno <104940937+Blazuno@users.noreply.github.com>
2025-02-20 23:47:54 -0500

    Update layer2bot.lua

--- layer2bot.lua
+++ layer2bot.lua
@@ -175,7 +175,7 @@ local function delete_chaser()
     local tween = fly_to(chaser.Torso.Position + Vector3.new(5,0,5), 100, chaser.Torso.Position)
     game.Workspace.Thrown.ChildAdded:Connect(function(c)
         if c.Name == "PerilousAttack" or c.Name == "SpikeStabEff" then 
-            wait(0.3)
+            wait(0.5)
             VIM:SendKeyEvent(true, 113, false, game)
             wait(0.05)
             VIM:SendKeyEvent(false, 113, false, game)
